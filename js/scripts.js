168d139038aa97a1af0a27a8ca0a5cebf035c595
Dorian <dorianoriginaldesings@gmail.com>
2021-09-08 01:30:42 +0200

    Ruleta arreglada 😊

--- js/scripts.js
+++ js/scripts.js
@@ -7,24 +7,28 @@ let randomNumberEnd = 0;
 
 const categories = {
   html: {
+    min: 0,
+    max: 90
+  },
+  css: {
     min: 270,
     max: 359
   },
-  css: {
-    min: 0,
-    max: 89
-  },
   js: {
-    min: 180,
-    max: 269
-  },
-  random: {
     min: 90,
     max: 179
+  },
+  random: {
+    min: 180,
+    max: 269
   }
 };
 
 const randomNumber = () => Math.floor(Math.random() * 360) + 1800;
+
+const fetchCategory = category => {
+  console.log(category);
+};
 
 const setCategory = () => {
   Object.keys(categories).forEach(category => {
@@ -32,18 +36,19 @@ const setCategory = () => {
       randomNumberEnd - 1800 >= categories[category].min &&
       randomNumberEnd - 1800 <= categories[category].max
     ) {
-      console.log(category);
+      fetchCategory(category);
     }
   });
 };
 
 rouletteButton.addEventListener('click', () => {
-  const generatedNumber = randomNumber();
-  rootStyle.setProperty('--spin-end', `${generatedNumber}deg`);
-  randomNumberEnd = generatedNumber;
-  rouletteSections.classList.add('roulette__sections--spin');
-  // console.log(setCategory());
-  setCategory();
+  if (!rouletteSections.classList.contains('roulette__sections--spin')) {
+    const generatedNumber = randomNumber();
+    rootStyle.setProperty('--spin-end', `${generatedNumber}deg`);
+    randomNumberEnd = generatedNumber;
+    rouletteSections.classList.add('roulette__sections--spin');
+    setCategory();
+  }
 });
 
 rouletteSections.addEventListener('animationend', () => {
